28fe82e0ada9febc324b768c203760ae73a8cb17
demikiwi <constandi.nicolas@gmail.com>
2020-04-17 10:50:12 +0200

    premier commit

---
+++ ClientSoap.php
@@ -0,0 +1,23 @@
+<?php
+
+$clientSoap = new SoapClient("https://etrs804-distance-constann.herokuapp.com/services/LaDistance?wsdl");
+$clientSoap->retourneDistance($a, $b, $c, $d);
+
+$lattitude_a = $_POST['lattitude_a'];
+$longitude_a = $_POST['longitude_a'];
+$lattitude_b = $_POST['lattitude_b'];
+$longitude_b = $_POST['longitude_b'];
+
+$distance = $clientSoap->__soapCall("retourneDistance", array($lattitude_a,$longitude_a,$lattitude_b,$longitude_b));
+
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Calcul de la distance</title>
+    </head>
+    <body>
+        <h2>Reponse du service Soap:</h2>
+        <p> La distance entre les deux villes est de <?php echo $distance; ?> Kilomètre<p>
+    </body>
+</html>
