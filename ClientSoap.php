99d698fb591b3d79977f3a066e9760997530b4d2
demikiwi <constandi.nicolas@gmail.com>
2020-04-17 15:40:11 +0200

    blip blop

--- ClientSoap.php
+++ ClientSoap.php
@@ -2,15 +2,16 @@
 if (!ini_get('php_soap')) {
     ini_set('php_soap', '1');
 }
-$clientSoap = new SoapClient("https://etrs804-distance-constann.herokuapp.com/services/LaDistance?wsdl");
-$clientSoap->retourneDistance($a, $b, $c, $d);
+phpinfo();
+//$clientSoap = new SoapClient("https://etrs804-distance-constann.herokuapp.com/services/LaDistance?wsdl");
+//$clientSoap->retourneDistance($a, $b, $c, $d);
 
 $lattitude_a = $_POST['lattitude_a'];
 $longitude_a = $_POST['longitude_a'];
 $lattitude_b = $_POST['lattitude_b'];
 $longitude_b = $_POST['longitude_b'];
 
-$distance = $clientSoap->__soapCall("retourneDistance", array($lattitude_a,$longitude_a,$lattitude_b,$longitude_b));
+//$distance = $clientSoap->__soapCall("retourneDistance", array($lattitude_a,$longitude_a,$lattitude_b,$longitude_b));
 
 ?>
 <html>
@@ -20,6 +21,6 @@ $distance = $clientSoap->__soapCall("retourneDistance", array($lattitude_a,$long
     </head>
     <body>
         <h2>Reponse du service Soap:</h2>
-        <p> La distance entre les deux villes est de <?php echo $distance; ?> Kilomètre<p>
+        <p> La distance entre les deux villes est de <?php //echo $distance; ?> Kilomètre<p>
     </body>
 </html>
